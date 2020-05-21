3ea53c78a806328a321d716742513462c4bf20c1
Ramzi Tannous <rtannous@twistbioscience.com>
2020-05-21 21:12:27 +0300

    update tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,6 @@
 pip install -U pip &&
 pip install -r requirements.txt && \
 cd api && \
-python manage.py waitdb && \
 python manage.py migrate --settings=settings.test && \
-python manage.py test --settings=settings.test && \
+python manage.py test --settings=settings.test --np-input && \
 rm -r media
