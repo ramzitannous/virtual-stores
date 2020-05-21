aaafc8d6b3f3068e5f5c2d7463af2657c18efdd5
Ramzi Tannous <rtannous@twistbioscience.com>
2020-05-21 21:33:04 +0300

    fix run test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,5 +2,5 @@ pip install -U pip &&
 pip install -r requirements.txt && \
 cd api && \
 python manage.py migrate --settings=settings.test && \
-python manage.py test --settings=settings.test --np-input && \
+python manage.py test --settings=settings.test --no-input && \
 rm -r media
